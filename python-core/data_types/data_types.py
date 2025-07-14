3a9f26b635c0652a02b3bae11ab9c7da4d4666ef
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-14 16:45:45 +0530

    data-types

---
+++ python-core/data_types/data_types.py
@@ -0,0 +1,13 @@
+# Object Types / Data Types
+
+# Number : 1234, 3.14, 3_5j, Decimal(), Fraction()
+# Spring : "spam", "Bob's"
+# List : [1, 2, 3], [], ["apple", "banana"], list(range(20))
+# Tuple : (1, 'spam', 4, 'U'), tuple('spam')
+# Dict : {'food': 'spam', 'taste': 'yum'}
+# Set : set('abc')
+
+# File - open('eggs.txt'), open()
+# Boolean - True/False
+# None : None
+# Functions, modules, classes
