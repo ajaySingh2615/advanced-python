6208a8c1ebd05d5beff730920539b626c2c93eaa
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-29 12:10:11 +0530

    Refactor movie ticket pricing script to improve readability and structure

--- python-core/conditional-programming/02_movie_ticket_pricing.py
+++ python-core/conditional-programming/02_movie_ticket_pricing.py
@@ -1,2 +1,11 @@
 # Movie tickets are priced based on age: $12 for adults (18 and over), $8 for children. Everyone gets a discount on wednesday.
 
+age = 26
+day = "wednesday"
+
+price = 12 if age >= 18 else 8
+
+if day == "wednesday":
+    price -= 2
+
+print("Ticket price for you is $", price)
